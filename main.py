309129abd3e564687b4945de2a728eecb6ae668c
Eron Padilla <erongpoliveira@gmail.com>
2026-06-08 02:30:57 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import pygame
+pygame.init()
+
+#Da set no tamanho da tela e tbm o nome do game
+tela = pygame.display.set_mode((1280 , 720))
+pygame.display.set_caption("Python Game")
+
+rodando = True
+
+#jogador
+player_x = 100
+player_y = 500
+player_largura = 50
+player_altura = 50
+
+vely = 0
+gravidade = 0.7
+forca_pulo = -15
+
+#Chao
+
+chao_y = 600
+
+
+#faz fechar
+while rodando:
+
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            rodando = False
+
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_SPACE:
+                # Só pula se estiver no chão
+                if player_y + player_altura >= chao_y:
+                    vely = forca_pulo
+    
+    
+    vely += gravidade
+    player_y += vely
+    if player_y + player_altura >= chao_y:
+        player_y = chao_y - player_altura
+        vely = 0
+
+
+    tecla = pygame.key.get_pressed()
+    if tecla[pygame.K_a]:
+        player_x -= 5
+    if tecla[pygame.K_d]:
+        player_x += 5
+    
+    
+    tela.fill((0,0,0))
+    pygame.draw.rect(tela, (100, 65, 154), (0, chao_y, 1280, 120))
+    pygame.draw.rect(tela, 
+                     (255, 0, 0), 
+                     (player_x, player_y, 
+                      player_largura, player_altura)
+    )
+
+    pygame.display.update()
+
+
+
+pygame.quit()
+
+def Sprite():
+    pass
